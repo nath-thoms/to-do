67a659ee600d1d99ee60fa026bfe23c206b5d94b
nath <nath.chadwick@gmail.com>
2018-08-03 12:55:03 +0100

    added onKey delete feature

--- src/com/nathan/todolist/Controller.java
+++ src/com/nathan/todolist/Controller.java
@@ -10,6 +10,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 import javafx.util.Callback;
@@ -115,7 +117,8 @@ public class Controller {
                                     } else {
                                         cell.setContextMenu(listContextMenu);
                                     }
-                            });
+                            }
+                    );
 
                     return cell;
             }
@@ -156,6 +159,16 @@ public class Controller {
         } else {
             System.out.println("Cancel pressed");
         }
+    }
+
+    @FXML
+    public void handleKeyPressed(KeyEvent keyEvent) {
+       ToDoItem selectedItem = todoListView.getSelectionModel().getSelectedItem();
+       if(selectedItem != null) {
+           if(keyEvent.getCode().equals(KeyCode.DELETE)) {
+               deleteItem(selectedItem);
+           }
+       }
     }
 
     @FXML
